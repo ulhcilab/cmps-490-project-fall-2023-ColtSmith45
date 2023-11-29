f424ba022fb16ac8172127f1bbbc73a109751097
Colt Smith <coltsmith1212@gmail.com>
2023-11-29 00:55:28 -0600

    Fixed A lot of Bugs

---
+++ Putt-Putt Pals/Assets/Scripts/Golf Ball.cs
@@ -0,0 +1,263 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GolfBall : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+    public static float maxPuttPower = .5f;
+    public float puttPowerIncrementSpeed = .5f;
+    private float puttPower = 0.0f;
+    private bool ballMoving = false;
+    public int totalPutts = 0;
+    public int putts = 0;
+    public bool isTurn = false;
+    private Rigidbody rb;
+    public TextMeshProUGUI puttPowerText;
+    public TextMeshProUGUI puttText;
+    public TextMeshProUGUI centerText;
+    public GameObject turnManager;
+    public GameObject puttPowerBar;
+    private static Image puttPowerBarImg;
+    public GameObject directionArrows;
+    public bool reachedHole = false;
+    public bool endCalled = false;
+    public AudioSource audioSource;
+    public AudioClip puttSoundEffect;
+    public AudioClip puttPowerSoundEffect;
+    public AudioClip holeSoundEffect;
+    public AudioClip successSoundEffect;
+    public AudioClip failureSoundEffect;
+    private bool audioPlaying = false;
+    private bool delay = false;
+    public Image centerTxtBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        centerTxtBack.enabled = false;
+        centerText.text = string.Empty;
+        audioSource = GetComponent<AudioSource>();
+        directionArrows.SetActive(false);
+        puttPowerBarImg = puttPowerBar.transform.GetComponent<Image>();
+        rb = GetComponent<Rigidbody>();
+        SetPuttPowerBarValue(puttPower);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTurn)
+        {
+
+            if (rb.velocity.magnitude > 0.0001)
+            {
+                ballMoving = true;
+            }
+            else
+            {
+                ballMoving = false;
+            }
+
+            //Ball is not moving and hasn't reached max putts
+            if (!ballMoving)
+            {
+                if (putts < TurnManager.maxPutts)
+                {
+                    if (!reachedHole)
+                    {
+                        //Debug.Log("reached Hole false");
+                        directionArrows.SetActive(true);
+                    }
+
+                    // rotate left
+                    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        if (!(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftShift))) Rotate(-rotationSpeed);
+                        else Rotate(-rotationSpeed / 6);
+                    }
+                    // rotate right
+                    else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                    {
+                        if (!(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftShift))) Rotate(rotationSpeed);
+                        else Rotate(rotationSpeed / 6);
+                    }
+
+                    // Check for space bar input to control putt power
+                    if (Input.GetKey(KeyCode.Space))
+                    {
+                        if (puttPower + puttPowerIncrementSpeed * Time.deltaTime > maxPuttPower)
+                        {
+                            puttPower = maxPuttPower;
+                        }
+                        else
+                        {
+                            puttPower += puttPowerIncrementSpeed * Time.deltaTime;
+                        }
+                        SetPuttPowerBarValue(puttPower);
+
+                        if (!audioPlaying)
+                        {
+                            audioPlaying = true;
+                            audioSource.volume = 0.5f;
+                            audioSource.pitch = (1 + puttPower) * audioSource.pitch;
+                            audioSource.PlayOneShot(puttPowerSoundEffect, 1);
+                            StartCoroutine(WaitForAudioFinish(puttPowerSoundEffect.length));
+                        }
+                    }
+
+                    // Check for space bar release to perform the putt
+                    if (Input.GetKeyUp(KeyCode.Space))
+                    {
+                        audioSource.volume = 1;
+                        audioSource.pitch = 1;
+                        audioSource.PlayOneShot(puttSoundEffect, 0.25f);
+                        GetComponent<Rigidbody>().AddForce(transform.forward * puttPower, ForceMode.Impulse);
+                        totalPutts++;
+                        putts++;
+                        puttText.text = "Putts: " + putts;
+                        puttPower = 0.0f;
+                        SetPuttPowerBarValue(puttPower);
+                        StartCoroutine(DelayForTime(1));
+                    }
+
+                    puttPowerText.text = "Putt Power: " + Mathf.Floor(puttPower * 200);
+
+                //Ball has reached max putts, has stopped moving, & hasn't reached the hole
+                }
+                else if (!reachedHole && !ballMoving && !delay)
+                {
+                    directionArrows.SetActive(false);
+                    StartCoroutine(ReachedMaxPutts());
+                }
+
+            //Ball moving and still has putts left 
+            } else
+            {
+                directionArrows.SetActive(false);
+            }
+        }
+    }
+
+    public static void SetPuttPowerBarValue(float value)
+    {
+        float mappedValue = value / maxPuttPower; // Map the value to the [0,1] range
+        puttPowerBarImg.fillAmount = mappedValue;
+        if (puttPowerBarImg.fillAmount < 0.3f)
+        {
+            SetPuttPowerBarColor(Color.green);
+        }
+        else if (puttPowerBarImg.fillAmount < 0.7f)
+        {
+            SetPuttPowerBarColor(Color.yellow);
+        }
+        else
+        {
+            SetPuttPowerBarColor(Color.red);
+        }
+    }
+
+    public static void SetPuttPowerBarColor(Color c)
+    {
+        puttPowerBarImg.color = c;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Hole Drop"))
+        {
+            audioSource.PlayOneShot(successSoundEffect, 0.15f);
+            audioSource.PlayOneShot(holeSoundEffect, 0.15f);
+        } else if (col.gameObject.CompareTag("Hole"))
+        {
+            reachedHole = true; 
+            StartCoroutine(ReachedHole());
+        }
+        else if (col.gameObject.CompareTag("Kill Box"))
+        {
+            Restart();
+        } 
+    }
+
+    IEnumerator ReachedHole()
+    {
+        isTurn = false;
+        centerTxtBack.enabled = true;
+        if (putts == 1)
+        {
+            centerText.text = "Hole in One!";
+        } else
+        {
+            centerText.text = "Putts: " + putts;
+        }
+        yield return new WaitForSeconds(3);
+        centerTxtBack.enabled = false;
+        centerText.text = "";
+        putts = 0;
+        gameObject.SetActive(false);
+        TurnManager.playersFinished++;
+        turnManager.GetComponent<TurnManager>().NextTurn();
+        reachedHole = false;
+        endCalled = false;
+    }
+
+    IEnumerator ReachedMaxPutts()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (!endCalled)
+        {
+            endCalled = true;
+            isTurn = false;
+            audioSource.PlayOneShot(failureSoundEffect, 0.15f);
+            totalPutts += TurnManager.maxPuttPenalty;
+            puttText.text = "Putts: " + (putts + TurnManager.maxPuttPenalty);
+            centerTxtBack.enabled = true;
+            centerText.text = "Max Putts Reached     Putts: " + (putts + TurnManager.maxPuttPenalty);
+            yield return new WaitForSeconds(3);
+            centerTxtBack.enabled = false;
+            centerText.text = "";
+            putts = 0;
+            gameObject.SetActive(false);
+            TurnManager.playersFinished++;
+            turnManager.GetComponent<TurnManager>().NextTurn();
+            reachedHole = false;
+            endCalled = false;
+        }
+    }
+
+    void Restart()
+    {
+        Debug.Log("Restart Called");
+        rb.velocity = Vector3.zero;
+        transform.position = turnManager.GetComponent<TurnManager>().holeLocations[turnManager.GetComponent<TurnManager>().hole];
+    }
+
+    IEnumerator WaitForAudioFinish(float audioLength)
+    {
+        yield return new WaitForSeconds(audioLength);
+        audioPlaying = false;
+    }
+
+    IEnumerator DelayForTime(float delayTime)
+    {
+        delay = true;
+        yield return new WaitForSeconds(delayTime);
+        delay = false;
+    }
+
+    void Rotate(float amount)
+    {
+        float targetRotation = transform.rotation.eulerAngles.y + amount;
+
+        // Use Mathf.LerpAngle for smooth interpolation between current and target rotation
+        float newRotation = Mathf.LerpAngle(transform.rotation.eulerAngles.y, targetRotation, Time.deltaTime * rotationSpeed);
+
+        // Apply the new rotation
+        transform.rotation = Quaternion.Euler(0, newRotation, 0);
+    }
+}
+
+
---
+++ Putt-Putt Pals/Assets/Scripts/Menu.cs
@@ -0,0 +1,491 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas startMenu;
+    public Canvas controlsScreen;
+    public Canvas creditsScreen;
+    public Canvas gameSetupScreen;
+    private int holeStart = 1;
+    private int holeEnd = 18;
+    private int maxPutts = 8;
+    private int maxPuttPenalty = 2;
+    private int playerCount = 1; 
+    public TextMeshProUGUI holeStartTxt;
+    public TextMeshProUGUI holeEndTxt;
+    public TextMeshProUGUI maxPuttsTxt;
+    public TextMeshProUGUI maxPuttPenaltyTxt;
+    public TextMeshProUGUI playerCountTxt;
+    public TextMeshProUGUI colorSlider1Txt;
+    public TextMeshProUGUI colorSlider2Txt;
+    public TextMeshProUGUI colorSlider3Txt;
+    public TextMeshProUGUI colorSlider4Txt;
+    public Button holeStartPlus;
+    public Button holeStartMinus;
+    public Button holeEndPlus;
+    public Button holeEndMinus;
+    public Button maxPuttsPlus;
+    public Button maxPuttsMinus;
+    public Button maxPuttPenaltyPlus;
+    public Button maxPuttPenaltyMinus;
+    public Button playerCountPlus;
+    public Button playerCountMinus;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+    public TMP_InputField player2Input;
+    public TMP_InputField player3Input;
+    public TMP_InputField player4Input;
+    public AudioClip clickSoundEffect;
+    private AudioSource audioSource;
+    private Tuple<String, String>[] colorHexPairs = new Tuple<string, string>[]
+    {
+        Tuple.Create("White", "#FFFFFF"),
+        Tuple.Create("Red", "#FF7F7F"),
+        Tuple.Create("Orange", "#FFB366"),
+        Tuple.Create("Yellow", "#FFFF66"),
+        Tuple.Create("Green", "#7FFF7F"),
+        Tuple.Create("Blue", "#7F7FFF"),
+        Tuple.Create("Purple", "#B366FF"),
+        Tuple.Create("Brown", "#8B4513"),
+        Tuple.Create("Gray", "#B3B3B3")
+    };
+    private string[] ballHexColors = {"#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"};
+    private string[] playerNames = { "Player 1", "Player 2", "Player 3", "Player 4" };
+    private int colorSlider1Index = 0;
+    private int colorSlider2Index = 0;
+    private int colorSlider3Index = 0;
+    private int colorSlider4Index = 0;
+    private float clickSoundLevel = 0.5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        //initialize screens enabled/disable states
+        controlsScreen.enabled = false;
+        creditsScreen.enabled = false;
+        gameSetupScreen.enabled = false;
+        startMenu.enabled = true; 
+
+        //Hide Non-added player boxes 
+        player2.SetActive(false);
+        player3.SetActive(false);
+        player4.SetActive(false);
+
+        //Assign default gamesetup values 
+        holeStartTxt.text = holeStart.ToString();
+        holeEndTxt.text = holeEnd.ToString();
+        maxPuttsTxt.text = maxPutts.ToString();
+        maxPuttPenaltyTxt.text = maxPuttPenalty.ToString();
+        playerCountTxt.text = playerCount.ToString();
+
+        //Assign starting gray arrows 
+        holeStartMinus.image.color = Color.gray;
+        holeEndPlus.image.color = Color.gray;
+        playerCountMinus.image.color = Color.gray;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (startMenu.enabled)
+            {
+                ExitButton();
+            } else 
+            {
+                BackButton();
+            }
+        }
+    }
+
+    public void BackButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (controlsScreen.enabled)
+        {
+            controlsScreen.enabled = false;
+
+        } else if (creditsScreen.enabled)
+        {
+            creditsScreen.enabled = false;
+
+        } else if (gameSetupScreen.enabled)
+        {
+            gameSetupScreen.enabled = false;
+        }
+        startMenu.enabled = true;
+    }
+
+    public void ControlsButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        startMenu.enabled = false;
+        controlsScreen.enabled = true;
+    }
+
+    public void CreditsButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        startMenu.enabled = false;
+        creditsScreen.enabled = true;
+    }
+
+    public void PlayButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        startMenu.enabled = false;
+        gameSetupScreen.enabled = true;
+    }
+
+    public void ExitButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void HoleStartMinusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (holeStart != 1)
+        {
+            holeStart--;
+            holeStartTxt.text = holeStart.ToString();
+            if (holeStart == 1) holeStartMinus.image.color = Color.gray;
+            else
+            {
+                holeStartMinus.image.color = Color.white;
+                holeStartPlus.image.color = Color.white;
+                holeEndMinus.image.color = Color.white;
+            }
+        } 
+    }
+    public void HoleStartPlusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (holeStart != holeEnd)
+        {
+            holeStart++;
+            holeStartTxt.text = holeStart.ToString();
+            holeStartMinus.image.color = Color.white;
+            if (holeStart == holeEnd)
+            {
+                holeStartPlus.image.color = Color.gray;
+                holeEndMinus.image.color = Color.gray;
+            }
+            else holeStartPlus.image.color = Color.white;
+        }
+    }
+
+
+    public void HoleEndMinusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (holeEnd != 1 && holeEnd != holeStart)
+        {
+            holeEnd--;
+            holeEndTxt.text = holeEnd.ToString();
+            holeEndPlus.image.color = Color.white;
+            if (holeEnd == holeStart)
+            {
+                holeEndMinus.image.color = Color.gray;
+                holeStartPlus.image.color = Color.gray;
+            }
+            else holeEndMinus.image.color = Color.white;
+        }
+    }
+    public void HoleEndPlusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (holeEnd != 18)
+        {
+            holeEnd++;
+            holeEndTxt.text = holeEnd.ToString();
+            holeEndMinus.image.color = Color.white;
+            if (holeEnd == 18)
+            {
+                holeEndPlus.image.color = Color.gray;
+                holeStartPlus.image.color = Color.white;
+            }
+            else
+            {
+                holeEndPlus.image.color = Color.white;
+                holeStartPlus.image.color = Color.white;
+            }
+        }
+    }
+
+
+    public void MaxPuttsMinusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (maxPutts != 1)
+        {
+            maxPutts--;
+            maxPuttsTxt.text = maxPutts.ToString();
+            maxPuttsPlus.image.color = Color.white;
+            if (maxPutts == 1) maxPuttsMinus.image.color = Color.gray;
+        }
+    }
+    public void MaxPuttsPlusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (maxPutts != 10)
+        {
+            maxPutts++;
+            maxPuttsTxt.text = maxPutts.ToString();
+            maxPuttsMinus.image.color = Color.white;
+            if (maxPutts == 10) maxPuttsPlus.image.color = Color.gray;
+        }
+    }
+
+
+    public void MaxPuttPenaltyMinusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (maxPuttPenalty != 1)
+        {
+            maxPuttPenalty--;
+            maxPuttPenaltyTxt.text = maxPuttPenalty.ToString();
+            maxPuttPenaltyPlus.image.color = Color.white;
+            if (maxPuttPenalty == 1) maxPuttPenaltyMinus.image.color = Color.gray;
+        }
+    }
+    public void MaxPuttPenaltyPlusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (maxPuttPenalty != 3)
+        {
+            maxPuttPenalty++;
+            maxPuttPenaltyTxt.text = maxPuttPenalty.ToString();
+            maxPuttPenaltyMinus.image.color = Color.white;
+            if (maxPuttPenalty == 3) maxPuttPenaltyPlus.image.color = Color.gray;
+        }
+    }
+
+
+    public void PlayerCountMinusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (playerCount != 1)
+        {
+            playerCount--;
+            playerCountTxt.text = playerCount.ToString();
+            playerCountPlus.image.color = Color.white;
+            if (playerCount == 1)
+            {
+                playerCountMinus.image.color = Color.gray;
+                player2.SetActive(false);
+                ballHexColors[1] = "#FFFFFF";
+                colorSlider2Index = 0;
+                colorSlider2Txt.text = colorHexPairs[0].Item1;
+                playerNames[1] = "Player 2";
+                player2Input.text = "";
+            } else if (playerCount == 2)
+            {
+                player3.SetActive(false);
+                ballHexColors[2] = "#FFFFFF";
+                colorSlider3Index = 0;
+                colorSlider3Txt.text = colorHexPairs[0].Item1;
+                playerNames[2] = "Player 3";
+                player3Input.text = "";
+            } else if (playerCount == 3)
+            {
+                player4.SetActive(false);
+                ballHexColors[3] = "#FFFFFF";
+                colorSlider4Index = 0;
+                colorSlider4Txt.text = colorHexPairs[0].Item1;
+                playerNames[3] = "Player 4";
+                player4Input.text = "";
+            }
+        }
+    }
+    public void PlayerCountPlusButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        if (playerCount != 4)
+        {
+            playerCount++;
+            playerCountTxt.text = playerCount.ToString();
+            playerCountMinus.image.color = Color.white;
+
+            if (playerCount == 2)
+            {
+                player2.SetActive(true);
+
+            } else if (playerCount == 3)
+            {
+                player3.SetActive(true);
+            } else if (playerCount == 4)
+            {
+                if (playerCount == 4) playerCountPlus.image.color = Color.gray;
+                player4.SetActive(true);
+
+            }
+        }
+    }
+
+
+    public void RightColorArrowButton1()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider1Index++;
+        if (colorSlider1Index == colorHexPairs.Length)
+        {
+            colorSlider1Index = 0;
+        }
+        colorSlider1Txt.text = colorHexPairs[colorSlider1Index].Item1;
+        ballHexColors[0] = colorHexPairs[colorSlider1Index].Item2;
+
+    }
+
+    public void LeftColorArrowButton1()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider1Index--;
+        if (colorSlider1Index == -1)
+        {
+            colorSlider1Index = colorHexPairs.Length - 1;
+        }
+        colorSlider1Txt.text = colorHexPairs[colorSlider1Index].Item1;
+        ballHexColors[0] = colorHexPairs[colorSlider1Index].Item2;
+
+    }
+
+
+    public void RightColorArrowButton2()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider2Index++;
+        if (colorSlider2Index == colorHexPairs.Length)
+        {
+            colorSlider2Index = 0;
+        }
+        colorSlider2Txt.text = colorHexPairs[colorSlider2Index].Item1;
+        ballHexColors[1] = colorHexPairs[colorSlider2Index].Item2;
+
+    }
+
+    public void LeftColorArrowButton2()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider2Index--;
+        if (colorSlider2Index == -1)
+        {
+            colorSlider2Index = colorHexPairs.Length - 1;
+        }
+        colorSlider2Txt.text = colorHexPairs[colorSlider2Index].Item1;
+        ballHexColors[1] = colorHexPairs[colorSlider2Index].Item2;
+
+    }
+
+
+    public void RightColorArrowButton3()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider3Index++;
+        if (colorSlider3Index == colorHexPairs.Length)
+        {
+            colorSlider3Index = 0;
+        }
+        colorSlider3Txt.text = colorHexPairs[colorSlider3Index].Item1;
+        ballHexColors[2] = colorHexPairs[colorSlider3Index].Item2;
+
+    }
+
+    public void LeftColorArrowButton3()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider3Index--;
+        if (colorSlider3Index == -1)
+        {
+            colorSlider3Index = colorHexPairs.Length - 1;
+        }
+        colorSlider3Txt.text = colorHexPairs[colorSlider3Index].Item1;
+        ballHexColors[2] = colorHexPairs[colorSlider3Index].Item2;
+
+    }
+
+
+    public void RightColorArrowButton4()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider4Index++;
+        if (colorSlider4Index == colorHexPairs.Length)
+        {
+            colorSlider4Index = 0;
+        }
+        colorSlider4Txt.text = colorHexPairs[colorSlider4Index].Item1;
+        ballHexColors[3] = colorHexPairs[colorSlider4Index].Item2;
+    }
+
+    public void LeftColorArrowButton4()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        colorSlider4Index--;
+        if (colorSlider4Index == -1)
+        {
+            colorSlider4Index = colorHexPairs.Length - 1;
+        }
+        colorSlider4Txt.text = colorHexPairs[colorSlider4Index].Item1;
+        ballHexColors[3] = colorHexPairs[colorSlider4Index].Item2;
+
+    }
+
+
+    public void grabPlayer1Name(string name)
+    {
+        playerNames[0] = name;
+    }
+
+    public void grabPlayer2Name(string name)
+    {
+        playerNames[1] = name;
+    }
+
+    public void grabPlayer3Name(string name)
+    {
+        playerNames[2] = name;
+    }
+
+    public void grabPlayer4Name(string name)
+    {
+        playerNames[3] = name;
+    }
+
+
+
+    public void StartGameButton()
+    {
+        audioSource.PlayOneShot(clickSoundEffect, clickSoundLevel);
+        TurnManager.holeStart = holeStart;
+        TurnManager.holeEnd = holeEnd;
+        TurnManager.maxPutts = maxPutts;
+        TurnManager.maxPuttPenalty = maxPuttPenalty;
+        TurnManager.playerCount = playerCount;
+        TurnManager.playersFinished = 0;
+        TurnManager.ballHexColors = ballHexColors;
+        TurnManager.playerNames = playerNames;
+        SceneManager.LoadScene("Game Scene");
+    }
+
+
+
+
+
+}
